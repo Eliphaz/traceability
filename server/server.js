795c9e00d4e08922fdfd4de22ab8978ac3c54a82
Eliphaz <eliahlander3@gmail.com>
2022-03-17 14:48:47 -0600

    added endpoint

--- server/server.js
+++ server/server.js
@@ -8,6 +8,28 @@ app.use(express.json())
 
 app.use(express.static(path.join(__dirname, '../public')))
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '75b5268c629e4a9b8d0b1d711ac472aa',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
+app.get('/', (req, res) => {
+    try {
+        nonExistentFunction();
+      } catch (error) {
+          console.log(error)
+        rollbar.log(error);
+        // expected output: ReferenceError: nonExistentFunction is not defined
+        // Note - error messages will vary depending on browser
+      }
+})
+
 const port = process.env.PORT || 5050
 
 app.listen(port, () =>{
